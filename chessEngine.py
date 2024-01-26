5abe4f71f431a2fec1f5fa4ba207bd37e01e84d4
maxchtse <tsechunghei97@outlook.com>
2024-01-26 18:07:09 +0800

    Create GameState Class and a 8x8 Chess board

---
+++ chessEngine.py
@@ -0,0 +1,11 @@
+"""
+This is a multi-line comment with docstrings
+
+print("Hello world")
+print("Hello universe")
+print("Hello everyone")
+"""
+
+print("Hello everyone")
+
+print("Hello everyone")
